e905e6b4f60ec34a1b22d8949df7d613ca001944
shushikata <shu.42111227@gmail.com>
2020-09-12 11:19:52 +0000

    add clinic-show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6716a92c00465829ecb5c3f30772a1e177223690546bb2ce6090cb84129ae2e066e17744ef9b0f7272aa872674d6e486a2420e7d918eae5773be4d2da2b3fd1c'
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,44 @@
 Rails.application.routes.draw do 
 
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
-  }
+  # -------------------- devise -----------------------------------------------
+    devise_for :admins, controllers: {
+      sessions:      'admins/sessions',
+      passwords:     'admins/passwords',
+      registrations: 'admins/registrations'
+    }
 
-  devise_for :members, controllers: {
-    sessions:      'members/sessions',
-    passwords:     'members/passwords',
-    registrations: 'members/registrations'
-  }
+    devise_for :members, controllers: {
+      sessions:      'members/sessions',
+      passwords:     'members/passwords',
+      registrations: 'members/registrations'
+    }
+  # -------------------- devise -----------------------------------------------
 
-  root to: 'member/clinics#top'
-  get 'clinics/about', to: 'member/clinics#about', as: 'clinics_about'
+  # -------------------- admin ------------------------------------------------
+    root to: 'member/clinics#top'
+    get 'clinics/about', to: 'member/clinics#about', as: 'clinics_about'
 
-  get 'admin/top', to: 'admin/members#top', as: 'admin_members_top'
+    get 'admin/top', to: 'admin/members#top', as: 'admin_members_top'
 
-  namespace :admin do
+    namespace :admin do
 
-    resources :members, only: [:index, :show, :edit, :update]
-    resources :clinics
-    resources :consultation_hours, only: [:create, :update, :destroy]
-    resources :genres, except: [:show]
-    
-  end
+      resources :members, only: [:index, :show, :edit, :update]
+      resources :clinics
+      resources :consultation_hours, only: [:create, :update, :destroy]
+      resources :genres, except: [:show]
+      
+    end
+  # -------------------- admin ------------------------------------------------
+  
+  # -------------------- member -----------------------------------------------
+    get 'member/my_calendar', to: 'member/events#my_calendar'
 
-  get 'member/my_calendar', to: 'member/events#my_calendar'
+    namespace :member do
+      
+      resources :members, only: [:show, :edit, :update]
+      resources :clinics, only: [:show, :index]
+      resources :events
 
-  namespace :member do
-    
-    resources :members, only: [:show, :edit, :update]
-    resources :clinics, only: [:show, :index]
-    resources :events
-
-  end
-
+    end
+  # -------------------- member -----------------------------------------------
 end
