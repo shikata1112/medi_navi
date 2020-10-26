638ad579dde32ce39f61f049428dcd7d5df9bfa5
shushikata <shu.42111227@gmail.com>
2020-10-26 09:28:18 +0000

    fix batch

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -16,8 +16,6 @@ class Coupon < ApplicationRecord
       if coupon.created_at + coupon.limit.minutes < time && coupon.is_valid == '有効'
         coupon.is_valid = '無効'
         coupon.save
-
-        
       end
     end
   end
