5bac9d797317b1a382e225c7bc7a67239768f722
shushikata <shu.42111227@gmail.com>
2020-11-27 13:37:28 +0000

    refactoring admin-clinics-controller

--- app/controllers/admin/clinics_controller.rb
+++ app/controllers/admin/clinics_controller.rb
@@ -9,15 +9,19 @@ class Admin::ClinicsController < ApplicationController
   end
 
   def create 
-    if @clinic = Clinic.create(clinic_params)
-        clinic_params[:genre_ids].each do |genre_id|
-          genre_map = GenreMap.new(genre_id: genre_id,clinic_id:@clinic.id)
-          genre_map.save
-        end
-        redirect_to admin_clinics_path
-    else
-      render 'new'
-    end
+    @clinic = Clinic.new(clinic_params)
+    clinic_collection = ClinicCollection.new(@clinic, clinic_params[:genre_ids])
+    clinic_collection.save!
+    # if @clinic = Clinic.create(clinic_params)
+    #     clinic_params[:genre_ids].each do |genre_id|
+    #       genre_map = GenreMap.new(genre_id: genre_id,clinic_id: @clinic.id)
+    #       genre_map.save
+    #     end
+    redirect_to admin_clinics_path
+    # else
+  rescue => e
+    render 'new'
+    # end
   end
 
   def index
---
+++ app/models/clinic_collection.rb
@@ -0,0 +1,17 @@
+class ClinicCollection
+  def initialize(clinic, genre_ids)
+    @clinic = clinic
+    @genre_ids = genre_ids
+  end
+
+  def save!
+    puts 'やっほ-!!'
+    c = Clinic.first
+    puts c.inspect
+    @clinic = Clinic.create(clinic_params)
+    clinic_params[:genre_ids].each do |genre_id|
+      genre_map = GenreMap.new(genre_id: genre_id,clinic_id: @clinic.id)
+      genre_map.save
+    end
+  end
+end
