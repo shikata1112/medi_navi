24082788b6382aa0a116eb11c2f7a6c76fdc566b
shushikata <shu.42111227@gmail.com>
2020-12-15 20:54:14 +0900

    refactoring/member-rooms-controller

--- app/controllers/member/rooms_controller.rb
+++ app/controllers/member/rooms_controller.rb
@@ -6,7 +6,6 @@ class Member::RoomsController < ApplicationController
     Entry.create!(member_id: params[:member_id], room_id: room.id)
     redirect_to member_room_path(room)
   end
-  
 
   def index
     # current_entries = current_member.entries.includes(:room)
@@ -14,14 +13,16 @@ class Member::RoomsController < ApplicationController
     # current_entries.each do |entry|
     #   my_room_ids << entry.room.id
     # end
-    @another_entries = Entry.eager_load(:member, :room).where(room_id: current_member.my_room_ids).where.not(member_id: current_member.id)
+    @another_entries = Entry.eager_load(:member, :room)
+                            .where(room_id: current_member.my_room_ids)
+                            .where.not(member_id: current_member.id)
   end
 
   def show
     @room = Room.find(params[:id])
     @message = Message.new
     @messages = @room.messages.includes(:member)
-    @another_entry = @room.entries.find_by('member_id != ?', current_member.id)
+    @entries = @room.entries.find_by('member_id != ?', current_member.id)
   end
 
 end
