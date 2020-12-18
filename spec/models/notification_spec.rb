e33a0131951bdf89d49ee9d46574d648d2cd1739
shushikata <shu.42111227@gmail.com>
2020-12-18 21:25:21 +0900

    rspec/member-notifications-controlller-index

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -23,10 +23,20 @@ RSpec.describe Notification, type: :model do
   describe ".update_attributes!" do
     before do
       @member1 = create(:member)
+      @member2 = create(:guest)
     end
 
-    it "description" do
-      
+    it "checkedがtrueに更新されること" do
+      Notification.create(
+        visited_id: @member1.id,
+        visiter_id: @member2.id,
+        checked: false
+      )
+
+      notifications = @member1.passive_notifications
+      notifications.update_attributes!(notifications)
+
+      expect(true).to eq Notification.find(1).checked
     end
   end
 end
