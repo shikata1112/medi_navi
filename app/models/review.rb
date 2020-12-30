fd8cd382f227b5fec793690e62a7bac5785bf56f
shushikata <shu.42111227@gmail.com>
2020-12-30 21:34:00 +0900

    delete-commentout

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,11 +6,4 @@ class Review < ApplicationRecord
   acts_as_taggable
 
   validates :title, :comment, :wating_time, :score, presence: true
-
-  # def save!(current_member, params_score)
-  #   self.member_id = current_member.id
-  #   self.score = params_score
-  #   save!
-  # end
-
 end
