77c4453c938b7c34433f32245a9d101d7cc97b84
shushikata <shu.42111227@gmail.com>
2021-01-19 02:22:21 +0900

    Inquiriesテーブルのカラムコメントの変更

--- db/migrate/20210117083426_create_inquiries.rb
+++ db/migrate/20210117083426_create_inquiries.rb
@@ -1,8 +1,8 @@
 class CreateInquiries < ActiveRecord::Migration[5.2]
   def change
     create_table :inquiries do |t|
-      t.string :title, null: false, comment: '問い合わせの題名'
-      t.text :content, null: false, comment: '問い合わせの本文'
+      t.string :title, null: false, comment: 'タイトル'
+      t.text :content, null: false, comment: '本文'
       t.references :member, null: false
 
       t.timestamps
