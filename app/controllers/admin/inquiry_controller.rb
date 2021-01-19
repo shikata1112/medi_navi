1fa448016460239f60b08326a7754312c6af8bf2
shushikata <shu.42111227@gmail.com>
2021-01-20 01:45:14 +0900

    管理者側の問い合わせ管理ページ（index, show）作成

---
+++ app/controllers/admin/inquiry_controller.rb
@@ -0,0 +1,11 @@
+class Admin::InquiryController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @inquiries = Inquiry.all
+  end
+
+  def show
+    @inquiry = Inquiry.find(params[:id])
+  end
+end
