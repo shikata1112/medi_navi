184e5d3fc0b6aca5de8151b8927ba3d3c713b14f
shushikata <shu.42111227@gmail.com>
2021-01-19 19:45:49 +0900

    inquiry.rbのモデル単体テスト

--- spec/factories/inquiries.rb
+++ spec/factories/inquiries.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :inquiry do
-    
+    title { 'test' }
+    content { 'これはtestです。' }
+    member_id {}
   end
 end
--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Inquiry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    member = build(:member)
+    @inquiry = build(:inquiry, member_id: member.id)
+  end
+
+  it "問い合わせの登録ができること" do
+    expect(@inquiry).to be_valid
+  end
+
+  it "titleが空の場合は登録できないこと" do
+    @inquiry.title = " "
+    expect(@inquiry).to be_invalid
+  end
+
+  it "contentが空の場合は登録できないこと" do
+    @inquiry.content = " "
+    expect(@inquiry).to be_invalid
+  end
 end
