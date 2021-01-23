b972c728c6d3ceb30d3aa13c9f511954caadda32
四方 秀 <67092021+shushikata@users.noreply.github.com>
2021-01-23 22:58:42 +0900

    Update inquiry_controller_spec.rb

--- spec/controllers/member/inquiry_controller_spec.rb
+++ spec/controllers/member/inquiry_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Member::InquiryController, type: :controller do
   
   describe "confirm" do
     it "/member/inquiry/confirmにアクセスできること" do
-      get :confirm
+      post :confirm
       expect(response).to have_http_status(200)
     end
   end
