329e4316de6687c907fd613a8bb300f209dba295
shushikata <shu.42111227@gmail.com>
2021-01-28 05:25:00 +0900

    管理者問い合わせ詳細画面の実装

--- spec/models/inquiry_spec.rb
+++ spec/models/inquiry_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Inquiry, :type => :model do
+RSpec.describe Inquiry, type: :model do
+  describe "delegations" do
+    it { is_expected.to delegate_method(:name).to(:member).with_prefix }
+    it { is_expected.to delegate_method(:email).to(:member).with_prefix }
+  end
 end
