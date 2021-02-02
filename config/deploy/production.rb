5a70ea67784f8a31c0502ea48b3f14bcc0a92708
shushikata <shu.42111227@gmail.com>
2021-02-03 00:33:44 +0900

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '54.199.40.234', user: 'ec2-user', roles: %w{app web db}
+server '192.168.0.250', user: 'ec2-user', roles: %w{app web db}
 
 # role-based syntax
 # ==================
